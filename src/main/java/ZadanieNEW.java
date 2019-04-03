e386d2b67e074899bebe7a694a6193eea8a118f6
natalya.peresutko <natalya.peresutko@russianpost.ru>
2019-04-03 12:40:54 +0300

    второй файл

---
+++ src/main/java/ZadanieNEW.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ZadanieNEW {
+
+    public static long chet;
+    public static long nochet;
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите число n");
+        long n = input.nextInt();
+        for (; n != 0; n = n / 10) {
+
+            if (n % 2 == 0)
+                chet++;
+            else
+                nochet++;
+        }
+        System.out.println("Количество четных " + chet + " - Количество нечетных " + nochet);
+    }
+}
